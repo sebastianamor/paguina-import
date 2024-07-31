ba99c0425e4e8901929354563e595fe65cc991d4
¨Jane <smoreira11@hotmail.com>
2024-07-31 22:56:38 +0900

    add foto

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import './Home.css';
-
+import pullman from './imagenes/pullman.jpg';
+import starkken from './imagenes/STARKKEN.jpg';
+import vermontt from './imagenes/varmontt.jpg'
 import { FcDiploma1 } from "react-icons/fc";
 
 function Home() {
   return (
     <section className="home">
-       <h2>Inicio</h2>
+       <h2>jmv import</h2>
       <p className='iniciopag'>
-        
+      Estamos dedicados a tener las mejores llantas, neumáticos y más para tu vehiculo
+      Buscando satisfacer la necesidad de personalizar, mejorar el estilo, rendimiento y performande de tu vehiculo
       </p>
       <hr></hr>
-      
-        
-        
         <div className="home-text">
           <div className="inicio" >
-      
-      
-        
+          <p>enviamos a todo chile por </p>
           </div>
           </div>
-          <hr></hr>
           <div className="home-item">
-         
+          <img  src={pullman} alt='pull' className='pullmac'/>
+          <img  src={starkken} alt='star' className='stark' />
+          <img  src={vermontt} alt='ver'  className='vermon' />
           <div className="home-text"></div>
           <p>
             
